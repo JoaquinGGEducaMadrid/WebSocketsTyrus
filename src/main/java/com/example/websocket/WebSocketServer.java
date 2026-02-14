61c7a236c283c5dff87aab749e9cb1643725379e
JoaquinGGEducaMadrid <131858640+JoaquinGGEducaMadrid@users.noreply.github.com>
2026-02-14 13:08:03 +0100

    Modify WebSocketServer to use dynamic port and address (Render)
    Updated WebSocket server to use environment variable for port and bind to all interfaces.

--- src/main/java/com/example/websocket/WebSocketServer.java
+++ src/main/java/com/example/websocket/WebSocketServer.java
@@ -5,11 +5,16 @@ import org.glassfish.tyrus.server.Server;
 public class WebSocketServer {
 
     public static void main(String[] args) {
-        Server server = new Server("localhost", 8080, "/", null, EchoEndpoint.class);
-
+    	//  Server server = new Server("localhost", 8080, "/", null, EchoEndpoint.class);
+    	String port = System.getenv("PORT");
+    	//String servidor = "localhost";
+    	 String servidor ="0.0.0.0";  
+    	if (port == null)
+    		port ="8080";
+    	Server server = new Server(servidor, Integer.parseInt(port), "/", null, EchoEndpoint.class);
         try {
             server.start();
-            System.out.println("Servidor WebSocket iniciado en ws://localhost:8080/ws");
+            System.out.println("Servidor WebSocket iniciado en ws://"+servidor+":"+port+"/ws");
             Thread.currentThread().join();
         } catch (Exception e) {
             e.printStackTrace();
@@ -18,4 +23,5 @@ public class WebSocketServer {
         }
     }
 }
+
 
